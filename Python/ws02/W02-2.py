621dd06d4c4f5637165534b1a145fcfa8e9b458e
schnell7 <git@github.com:logmoc/msc-phygeo-class-of-2017-schnell7.git>
2017-11-09 17:29:01 +0100

    final

--- Python/ws02/W02-2.py
+++ Python/ws02/W02-2.py
@@ -17,29 +17,35 @@ vsni=0
 sni=0
 mni=0
 wni=0
+
+elinter = 0
 for j in range(0,len(oni)):
     if oni['ANOM'][j] > 2:
         #print("verystrong")
         vs=vs+1
-    elif oni['ANOM'][j] <= 1.9:
+    elif oni['ANOM'][j] > 1.5:
         #print("strong")
         s=s+1
-    elif oni['ANOM'][j] <= 1.4:
+    elif oni['ANOM'][j] > 1:
         #print("medium")
         m=m+1
-    elif oni['ANOM'][j] <= 0.9:
+    elif oni['ANOM'][j] > 0:
         #print("weak")
         w=w+1
     elif oni['ANOM'][j] < (-2):
         vsni=vsni+1
-    elif oni['ANOM'][j] >= (-1.9):
+    elif oni['ANOM'][j] < (-1.5):
         sni=sni+1
-    elif oni['ANOM'][j] >= (-1.4):
+    elif oni['ANOM'][j] < (-1):
         mni=mni+1
-    elif oni['ANOM'][j] >= (-0.9):
+    elif oni['ANOM'][j] < (0):
         wni=wni+1
+    elif oni['ANOM'][j] == 0:
+        elinter = elinter+1	
 print("EL NINO : very strong:",vs,"strong:",s,"medium:",m,"weak:",w)
-print( "EL NINA : very strong", vsni,"strong:",sni,"medium:",mni,"weak:",wni)
+print( "EL NINA : very strong:", vsni,"strong:",sni,"medium:",mni,"weak:",wni)
+print("ANO=0:   ", elinter)
+print("Anzahl Beobachtungen:", len(oni))
 #Aufgabe 2: Anteil der schwachen el nino events
 
 print((w*100)/(vs+s+m+w), "% der el nino events sind 'schwach' ")
@@ -48,3 +54,4 @@ print((w*100)/(vs+s+m+w), "% der el nino events sind 'schwach' ")
 
 events = {"weakNINA" : wni, "medNINA" : mni, "strongNINA" : sni, "vsNINA" : vsni, "weakNINO" : w, "medNINO" : m, "strongNINO" : s, "vsNINO" : vs}
 print(events)
+
