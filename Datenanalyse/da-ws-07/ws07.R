9694476a786861240083bfa20b079acb5f06c1ab
schnell7 <git@github.com:logmoc/msc-phygeo-class-of-2017-schnell7.git>
2017-12-18 16:44:14 +0100

    final

---
+++ Datenanalyse/da-ws-07/ws07.R
@@ -0,0 +1,51 @@
+anscombe
+names(anscombe)
+
+
+
+getwd()
+setwd("Dokumente/UniMR/GIT/Datenanalyse/data/")
+
+tab = read.csv("feldfrucht.csv")
+dep = names(tab)[7]
+vars = names(tab)[8:16]
+
+
+test = function(dep, vars, dataset = tab){
+  formel = paste(dep, "~", paste(vars, collapse = " + "))
+  lmod = lm(formel, data = dataset )
+  return(summary(lmod)$adj.r.squared)
+}
+
+matze = data.frame()
+for(i in 1:length(vars)){
+  a = paste(vars[-i], collapse = "+")
+  b = test(dep, vars[-i], dataset = tab )
+  a -> matze[i,1]
+  b -> matze[i,2]
+}
+
+# Gesamtmodell wird an letzte stelle von matze gestellt
+a = paste(vars, collapse = "+")
+b = test(dep, vars, dataset = tab)
+a -> matze[length(vars)+1,1]
+b -> matze[length(vars)+1,2]
+
+#matze[which(matze[,2] == max(matze[,2])),]
+neuevars = unlist(strsplit(matze[which(matze[,2] == min(matze[,2])),1], split = "+", fixed = T))
+
+if(matze[dim(matze)[1],2] > matze[dim(matze)[1]-1,2] ){
+  print("STOPP!")
+}
+
+
+while(matze[dim(matze)[1], 2] < max(matze[dim(matze)[1]-1, 2]) && length(neuevars) > 1) {
+  for (i in 1:length(neuevars)) {
+    a = paste(neuevars[-i], collapse = "+")
+    b = test(dep, neuevars[-i], dataset = tab)
+    a -> matze[i, 1]
+    b -> matze[i, 2]
+    
+  }
+  neuevars = unlist(strsplit(matze[which(matze[, 2] == min(matze[, 2])), 1], split = "+", fixed = T))
+}
