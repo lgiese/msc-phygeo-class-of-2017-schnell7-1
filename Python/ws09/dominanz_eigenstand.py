812dfbba51e1b32eb43ac51afe03a27936fb0abd
schnell7 <schnell7@students.uni-marburg.de>
2018-01-26 00:47:02 +0100

    final

--- Python/ws09/dominanz_eigenstand.py
+++ Python/ws09/dominanz_eigenstand.py
@@ -3,33 +3,35 @@
 import math
 import gdal, ogr, osr, os
 import numpy as np
-# props an https://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html#replace-no-data-value-of-raster-with-new-value
+
+# nach https://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html#replace-no-data-value-of-raster-with-new-value
 def raster2array(rasterfn):
     raster = gdal.Open(rasterfn)
     band = raster.GetRasterBand(1)
     return band.ReadAsArray()
 
 #%%
-dgm = gdal.Open("D:/UniData/py/subalpen.tif")
-#%%
-ar = raster2array("D:/UniData/py/subalpen.tif")
+ar = raster2array("./subalpen.tif")
 #%%
 #gipfelsuche entweder im gis oder hiermit
-for y in range(len(ar)):
-    print(y, max(ar[y]))
-#%%
-#vermeintliche gipfelhoehe als zahl eingeben
-#dateityp des arrays (float32) ist anders als float (2041.27 z.B.), logische abfragen in py daher immer FALSE
-for y in range(len(ar)):
-    if round(float(max(ar[y])), 2) == 2041.27 :
+def maxprozeile(ar):
+    for y in range(len(ar)):
         print(y, max(ar[y]))
+#%%
+
+#dateityp des arrays (float32) ist anders als float (2041.27 z.B.), logische abfragen in py daher immer FALSE
+#wo ist die zelle mit 'wert' gerundet auf nachkommastelle r in dgm_ar
+    #koennen auch mehrere sein!
+def woist(wert, r, dmg_ar):
+    for y in range(len(ar)):
         for x in range(len(ar[0])):
-            if round(float(ar[y][x]), 2) == 2041.27:
+            if round(float(ar[y][x]), r) == wert:
                 print("zeile:", y, "spalte:", x)
+                return([y, x])
     
 #%%
-#nur so
-dgm = gdal.Open("D:/UniData/py/subalpen.tif")
+gipf = woist(2041.27, 2, ar)
+
 
 #%%
 #z,s, gipfelkoordinaten, y,x von dominanz()
@@ -40,15 +42,14 @@ def distanz(z,s,y,x):
 #zeile, spalte(des gipfels), array des dgm, step grosse des fensters minimale dis wird ausgesucht, res = zellengroesse
 def dominanz(z,s, dgm_ar, step, res):
     end_list = []
-    di = 1
     for y in range(z-step, z+step+1):
         for x in range(s-step, s+step+1):
             if dgm_ar[z][s] < dgm_ar[y][x]:
                 dissi = distanz(z,s,y,x)
-                end_list.append(dissi)
-                
+                end_list.append(dissi)                
     if len(end_list)> 0:
-        print(end_list)
+        #print(end_list)
+        print("dominanz: ", min(end_list)*res)
         return(min(end_list)*res)
     else:
         print("""Alarm!Alarm!Alarm!Alarm!Alarm!Alarm! \n\n       setze 'step' groesser!\n\n====================================""")
@@ -73,17 +74,22 @@ def eigenstand(h, d, p):
   return(E)  
   
 #%%
+def estand(y,x,ar,step,res,p):
+    h = float(ar[y][x])
+    d = dominanz(y,x,ar,step,res)
+    print("eigenstand: ", eigenstand(h,d,p))
+    return(eigenstand(h,d,p))
+    
+
+#%%
  #300 wird hier yum test angenommen
  #gipfel hat nahegelegen hoeheren gipfelnachbar deswegen reicht 5 (3 ginge auch)
-h = float(ar[78][60])
-d = dominanz(78,60,ar,5,200)
-eigenstand(h, d, 300)
+
+estand(78,60,ar,5,200,300)
 #%%
 #naechster hoeherer gipfel viel wieter weg; 
-h2 = float(ar[80][65])
-d2 = dominanz(80,65,ar,64,200)
-eigenstand(h2, d2, 300)
 
+estand(80,65,ar,65,200,300)
 #%%
 #der Ansatz, das Fenster schrittweise zu vergroessern ist nicht ideal:
 #liegt der erste Punkt > Gipfel in der diagonalen des Quadrats ist die Entfernung
